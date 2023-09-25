0c8473d8fd0654ed4197001dc147a40a7f2b422d
Giugiumica <alexandru.costa@student.upt.ro>
2023-09-25 19:23:46 +0300

    ion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Nimeni nu stie");
+        System.out.println("problema 1 tema");
+        Scanner scanner=new Scanner(System.in);
+        int perimetru;
+        System.out.println("Introdu latura mare : ");
+        int L= scanner.nextInt();
+        System.out.println("Introdu latura mica : ");
+        int l= scanner.nextInt();
+
+
+        perimetru=2*l+2*L;
+        System.out.println("Perimetrul este : "+perimetru);
+
     }
 }
